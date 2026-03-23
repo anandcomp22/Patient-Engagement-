7fd9e5d4d0a2ca03e77c44e53f3499ae5f3601f1
Sayyoni-Parate <130883234+Sayyoni-Parate@users.noreply.github.com>
2026-03-23 18:11:07 +0530

    Delete download_model.py

--- download_model.py
+++
@@ -1,8 +0,0 @@
-from huggingface_hub import hf_hub_download
-
-model_path = hf_hub_download(
-    repo_id="Sayyoni/cataract_vgg16_model",
-    filename="cataract_vgg16_model.h5"
-)
-
-print("Model downloaded at:", model_path)
